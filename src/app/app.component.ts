a30d59cd892052eaafef5665b1cf26d8cb3b3440
canserkanuren <canserkan.uren@gmail.com>
2021-07-24 11:39:37 +0200

    :tada:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import '@raydeeeen/ui-component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import {
+  ApplicationRef,
+  CUSTOM_ELEMENTS_SCHEMA,
+  DoBootstrap,
+  NgModule
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { defineCustomElements } from '@raydeeeen/ui-component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  ngDoBootstrap(appRef: ApplicationRef): void {
+    defineCustomElements(window);
+  }
+}
